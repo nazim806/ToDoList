639d73ae9715e56947ac27da1f4ab2a9950c5634
Nazim Uddin <umnazim@gmail.com>
2020-04-16 13:39:03 -0700

    passing data from webpage to server

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const ejs = require("ejs");
 
 const app = express();
 
+var items = [];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -46,7 +50,13 @@ app.get("/", function (req, res) {
   //       console.log("Error: current day is : " + currentDay);
   //   }
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
