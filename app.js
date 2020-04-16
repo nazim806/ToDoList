e5db7d39a06ae39ac74051cd5c2cf261c3e17353
Nazim Uddin <umnazim@gmail.com>
2020-04-16 02:21:43 -0700

    ejs install and create views folder and list.ejs file

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  if (currentDay === 6 || currentDay === 0) {
+    day = "Weekend";
+  } else {
+    day = "Weekday";
+  }
+  res.render("list", { kindOfDay: day });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
