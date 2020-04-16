e2905914753b7ca7362802aaf9fbd166436568af
Nazim Uddin <umnazim@gmail.com>
2020-04-16 14:11:30 -0700

    public folder and express app use static file inside public folder

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let items = ["Buy grocery", "Cook food", "Enjoy food"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   let today = new Date();
