8a3d70933ed2d773465927d30c337dac90ad3144
Nazim Uddin <umnazim@gmail.com>
2020-04-16 03:11:19 -0700

    ejs template, form, submit button

--- app.js
+++ app.js
@@ -11,32 +11,40 @@ app.get("/", function (req, res) {
   var currentDay = today.getDay();
   var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    default:
-      console.log("Error: current day is : " + currentDay);
-  }
+  var day = today.toLocaleDateString("en-US", options);
+
+  //   switch (currentDay) {
+  //     case 0:
+  //       day = "Sunday";
+  //       break;
+  //     case 1:
+  //       day = "Monday";
+  //       break;
+  //     case 2:
+  //       day = "Tuesday";
+  //       break;
+  //     case 3:
+  //       day = "Wednesday";
+  //       break;
+  //     case 4:
+  //       day = "Thursday";
+  //       break;
+  //     case 5:
+  //       day = "Friday";
+  //       break;
+  //     case 6:
+  //       day = "Saturday";
+  //       break;
+
+  //     default:
+  //       console.log("Error: current day is : " + currentDay);
+  //   }
 
   res.render("list", { kindOfDay: day });
 });
