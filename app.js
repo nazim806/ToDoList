276068f3f3203a8b50ec5e98ab22fa64967d8aa0
Nazim Uddin <umnazim@gmail.com>
2020-04-16 21:48:54 -0700

    create date file, export module to app

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy grocery", "Cook food", "Enjoy food"];
-let workItems = [];
+const items = ["Buy grocery", "Cook food", "Enjoy food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -13,49 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  var currentDay = today.getDay();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
-  //   switch (currentDay) {
-  //     case 0:
-  //       day = "Sunday";
-  //       break;
-  //     case 1:
-  //       day = "Monday";
-  //       break;
-  //     case 2:
-  //       day = "Tuesday";
-  //       break;
-  //     case 3:
-  //       day = "Wednesday";
-  //       break;
-  //     case 4:
-  //       day = "Thursday";
-  //       break;
-  //     case 5:
-  //       day = "Friday";
-  //       break;
-  //     case 6:
-  //       day = "Saturday";
-  //       break;
-
-  //     default:
-  //       console.log("Error: current day is : " + currentDay);
-  //   }
+  const day = date.getDate();
 
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -71,7 +36,7 @@ app.get("/work", function (req, res) {
 });
 
 app.post("/work", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
