77751ad197fe84f5af758115249b748cb2fe9022
Nazim Uddin <umnazim@gmail.com>
2020-04-17 23:46:27 -0700

    add new item to todolistDB

--- app.js
+++ app.js
@@ -56,15 +56,14 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  
+  const item = new Item ({
+    name:itemName
+  });
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  item.save();
+  res.redirect("/");  
 });
 
 app.get("/work", function (req, res) {
